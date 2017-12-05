4e8be7324a8331a3b485a6704bf9f0d686373974
jamesrhee91 <jamesshrhee@gmail.com>
2017-12-04 21:12:51 -0500

    fixed reveal effect

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,33 +1,40 @@
 import React from 'react'
-import img from '../images/ss.png'
-import { Button } from 'semantic-ui-react'
+import img from '../images/Untitled.png'
+import { Button, Reveal, Image } from 'semantic-ui-react'
 
 const Portfolio = () => {
   return (
     <div className="container" id="portCont">
       <h1 className="heading" id="portfolio">Portfolio</h1>
       <div className="cardsCont">
+
           <div className="card-wrapper">
             <div className="card">
               <img src={img} alt="PARX" id="test" />
-              <div id="overlay">test</div>
+              <div id="overlay"></div>
+              <div id="button">
+                <a href="#" id="link">VIEW PROJECT</a>
+              </div>
             </div>
           </div>
+
           <div className="card-wrapper">
             <div className="card">
-              <img src={img} alt="WeOrder" id="test" />
-              <div id="overlay">test</div>
+              <img src={img} alt="PARX" id="test" />
+              <div id="overlay"></div>
+              <div id="button">
+                <a href="#" id="link">VIEW PROJECT</a>
+              </div>
             </div>
           </div>
+
           <div className="card-wrapper">
             <div className="card">
               <img src={img} alt="Piano Tiles" id="test" />
-              <div id="overlay">
-
-                  <h4>Piano Tiles</h4>
-                  <div className="desc">
-                  <Button inverted >View Project</Button><div>Play</div>
-                  </div>
+              <div id="overlay"></div>
+              <h4 className="title">PIANO TILES</h4>
+              <div id="button">
+                <a href="#" id="link">VIEW PROJECT</a>
               </div>
             </div>
           </div>
